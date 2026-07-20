5073608f6da260543184614938aa60b4f4f8d748
abdulraheem-tk <abdulraheem@codesuite.org>
2026-07-20 13:05:47 +0500

    feat: add sitemap configuration for SEO optimization

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = "https://aliwaris.netlify.app";
+    const baseUrl = "https://aliwarisdev.co.uk";
 
     return [
         {
